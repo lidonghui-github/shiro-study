878a844efc28910f22753cd7e36c71fbe9e00f23
lidonghui-github <1161656983@qq.com>
2021-06-27 00:02:37 +0800

    自定义realm

---
+++ src/main/java/cn/hd/realm/MyRealm.java
@@ -0,0 +1,35 @@
+package cn.hd.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * @author lidonghui
+ * @date 2021-06-26 23:53:49
+ * @desc ...
+ */
+public class MyRealm  extends AuthorizingRealm {
+    //授权方法
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    //认证方法
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        //得到传入的用户名
+        String principal = (String) token.getPrincipal();
+        //将传入的用户名与数据库中的比较
+        if("lidonghui".equals(principal)){
+            SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(principal, "123456", this.getName());
+            return simpleAuthenticationInfo;
+        }
+        return null;
+    }
+}
--- src/main/java/cn/hd/realm/TestMtRealm.java
+++ src/main/java/cn/hd/realm/TestMtRealm.java
@@ -1,27 +1,25 @@
-package cn.hd;
+package cn.hd.realm;
 
 import org.apache.shiro.SecurityUtils;
-
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.mgt.DefaultSecurityManager;
-import org.apache.shiro.realm.text.IniRealm;
 import org.apache.shiro.subject.Subject;
 
 /**
  * @author lidonghui
- * @date 2021-06-26 21:33:51
+ * @date 2021-06-26 23:58:14
  * @desc ...
  */
-public class TestAuthenticator {
+public class TestMtRealm {
     public static void main(String[] args) {
         DefaultSecurityManager securityManager = new DefaultSecurityManager();
-        securityManager.setRealm(new IniRealm("classpath:shiro.ini"));
+        securityManager.setRealm(new MyRealm());
         SecurityUtils.setSecurityManager(securityManager);
         Subject subject = SecurityUtils.getSubject();
         UsernamePasswordToken token = new UsernamePasswordToken("lidonghui", "123456");
         subject.login(token);
-        System.out.println(subject.isAuthenticated());
         System.out.println(subject.getPrincipal());
+        System.out.println(subject.isAuthenticated());
 
     }
 }
